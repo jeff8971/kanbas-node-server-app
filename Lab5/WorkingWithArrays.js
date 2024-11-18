59027ba25490774e79d4db81f0e2cebccf1d865e
jeff8971 <zhao.yuan2@northeastern.edu>
2024-11-17 23:49:38 -0500

    3.43

---
+++ Lab5/WorkingWithArrays.js
@@ -0,0 +1,27 @@
+let todos = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+  { id: 3, title: "Task 3", completed: false },
+  { id: 4, title: "Task 4", completed: true },
+];
+
+export default function WorkingWithArrays(app) {
+  app.get("/lab5/todos", (req, res) => {
+    //if called by query 
+    const { completed } = req.query;
+    if (completed !== undefined) {
+        const completedBool = completed === "true";
+        const completedTodos = todos.filter(
+            (t) => t.completed === completedBool);
+        res.json(completedTodos);
+        return;
+    }
+    //if no query sent
+    res.json(todos);
+});
+  app.get("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    res.json(todo);
+  });
+}
--- Lab5/index.js
+++ Lab5/index.js
@@ -1,6 +1,7 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 
 
 export default function Lab5(app) {
@@ -10,4 +11,6 @@ export default function Lab5(app) {
   PathParameters(app);
   QueryParameters(app);
   WorkingWithObjects(app);
+  WorkingWithArrays(app);
+
 };
