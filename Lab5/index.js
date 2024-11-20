08e7beff2d744e0781f43d0d2973c9d76e742e88
jeff8971 <zhao.yuan2@northeastern.edu>
2024-11-20 09:27:14 -0500

    3.5.6 done

--- Lab5/index.js
+++ Lab5/index.js
@@ -8,6 +8,7 @@ export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
   res.send("Welcome to Lab 5");
   });
+  
   PathParameters(app);
   QueryParameters(app);
   WorkingWithObjects(app);
