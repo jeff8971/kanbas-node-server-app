a1588d819ec79ced438032afce2c64bcccbb8f34
jeff8971 <zhao.yuan2@northeastern.edu>
2024-12-03 00:59:12 -0500

    temp before 4

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 import "dotenv/config";
+import cors from "cors";
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from "mongoose";
+import session from "express-session";
+
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
-import UserRoutes from "./Kanbas/Users/routes.js";
-import session from "express-session";
-import "dotenv/config";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
-import PoepleRoutes from './Kanbas/People/routes.js';
+import UserRoutes from "./Kanbas/Users/routes.js";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+const app = express();
+
+dotenv.config();
+
 mongoose.connect(CONNECTION_STRING);
+app.use(express.json());
 
-// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-// mongoose.connect(CONNECTION_STRING);
+app.use(cors());
 
-const app = express()
-
-
-app.use(cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-}));
 const sessionOptions = {
-    secret: "any string",
+    secret: process.env.SESSION_SECRET || "xr7kHlX6IX6HQHI",
     resave: false,
     saveUninitialized: false,
 };
@@ -37,19 +35,17 @@ if (process.env.NODE_ENV !== "development") {
         secure: true,
         domain: process.env.NODE_SERVER_DOMAIN,
     };
-}  
+}
 app.use(
     session(sessionOptions)
 );
-  
-app.use(express.json());
 
 Hello(app);
 Lab5(app);
-UserRoutes(app);
-CourseRoutes(app);
-ModuleRoutes(app);
 AssignmentRoutes(app);
-PoepleRoutes(app);
+CourseRoutes(app);
+EnrollmentsRoutes(app);
+ModuleRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000)
