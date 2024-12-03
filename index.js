280bb63a1586240e0b25607da1ad4d2b71da0b77
jeff8971 <zhao.yuan2@northeastern.edu>
2024-12-02 22:16:38 -0500

    3001->3000

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3001",
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
 }));
 const sessionOptions = {
     secret: "any string",
